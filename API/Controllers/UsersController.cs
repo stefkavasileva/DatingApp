d47ed6a84da4d80c5808b8a3b1990fb943793cc4
stefkavasileva <stefkavasileva93@gmail.com>
2021-03-08 13:33:10 +0200

    init

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+    }
+}
